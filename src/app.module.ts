79e6196f44902fc4c35fd55d0e389a46980b50e0
sbabaiev <serhii@nexar.app>
2023-02-15 13:19:30 +0200

    Commit final of course

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson, Student],
+    }),
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+    StudentModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/student/student.resolver.ts
@@ -0,0 +1,26 @@
+import { StudentService } from './student.service';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { StudentType } from './student.type';
+import { CreateStudentInput } from './create-student.input';
+
+@Resolver((of) => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Query((returns) => StudentType)
+  async student(@Args('id') id: string) {
+    return this.studentService.getStudent(id);
+  }
+
+  @Query((returns) => [StudentType])
+  async students() {
+    return this.studentService.getAllStudents();
+  }
+
+  @Mutation((returns) => StudentType)
+  async createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(createStudentInput);
+  }
+}
